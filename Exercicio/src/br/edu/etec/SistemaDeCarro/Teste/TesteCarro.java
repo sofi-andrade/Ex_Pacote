65a319d1d9660c5ba28b2a0265101470990affcf
sofi-andrade <sofiaisabelleandraderocha@gmail.com>
2026-05-07 20:41:34 -0300

    Add files via upload

---
+++ Exercicio/src/br/edu/etec/SistemaDeCarro/Teste/TesteCarro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.etec.SistemaDeCarro.Teste;
+
+import br.edu.etec.SistemaDeCarro.model.Carro;
+
+/**
+ *
+ * @author Admin
+ */
+public class TesteCarro {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Carro carro = new Carro("Vermelho", "18238923", "BMW", "X5");
+        carro.info();
+}
+}
